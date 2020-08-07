40ed45a59545eb317c99eee6f280b8757111e716
Mary Brown <marycbrown2004@gmail.com>
2020-08-07 19:51:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+dna1 = input('Please enter a string of C,A,G,and T: ')
+
+letters = ['C', 'A', 'G', 'T', 'c', 'a', 'g', 't']
+
+if dna1 == (letters):
+  print('something')
+else:
+  print('Invalid Input')
+  print(input('Please enter a string of C,A,G,and T: '))
+
+
+dna2 = input('Please enter another string of C,A,G,and T: ')
+
+if dna2 == (letters):
+  print('something')
+else:
+  print('Invalid Input')
+  print(input('Please enter another string of C,A,G,and T: '))
+
+#I cant get it to work
